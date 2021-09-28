97e9fe7fe28f1d207310f5adcfcd6ab1340b71ef
jaydeep <jaedeepmane4329@gmail.com>
2021-09-28 15:38:10 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 
 // import './App.css';
 // import Contact from './Components/Contact';
-// import Header from './Components/Header';
+import Header from './Components/Header';
 // import Projects from './Components/Projects';
-// import Skills from './Components/Skills';
+import Skills from './Components/Skills';
 import Title from './Components/Title';
 
 
@@ -14,8 +14,8 @@ function App() {
   return (
        <>
          <Title />
-          {/* <Header />  */}
-         {/* <Skills  /> */}
+          <Header /> 
+         <Skills  />
          {/* <Projects /> */}
           {/* <Contact />  */}
        </>
